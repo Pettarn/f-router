a175a62210cff46145adabbb712ca47f1488c5c1
Pettarn <1225076678@qq.com>
2019-09-28 00:36:19 +0800

    add solve idea

--- test.js
+++ test.js
@@ -1,21 +1,4 @@
-let arr = [
-    {
-        age: 1,
-        name: '1'
-    },
-    {
-        age: 2,
-        name: '2',
-    },
-    {
-        age: 3,
-        name: '3',
-    }
-]
+let test = "<div \
+</div>"
 
-let arr2 = []
-arr.forEach(item => {
-    arr2.push(item.age)
-})
-
-console.log(arr2)
+console.log(test)
