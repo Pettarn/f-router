06ed23e8e6204da496c7899018f94f1b199b0b22
Pettarn <1225076678@qq.com>
2019-09-28 12:12:30 +0800

    add router-view template

---
+++ template/router-view.vue
@@ -0,0 +1,23 @@
+<template>
+    <div :id="currentPath">
+
+    </div>
+</template>
+
+<script>
+
+let currentComponent = this.$route.component
+
+new Vue.extend(currentComponent).$mount(`#${currentPath}`)
+
+export default {
+    data() {
+        return {
+            currentPath: this.$route.path
+        }
+    },
+    components: {
+        currentComponent
+    }
+}
+</script>
