4849969cd952dc9417e7686971b0a942edbb08e4
Pettarn <1225076678@qq.com>
2019-10-10 16:02:46 +0800

    register this.$router and this.$route

--- core/install.js
+++ core/install.js
@@ -1,7 +1,24 @@
 import FRouter from './index.js'
+import RouterView from '../template/router-view.js'
+import RouterLink from '../template/router-link.js'
 
+
+
+let isDef = item => item !== undefined
 
 FRouter.install = function (Vue) {
+
+    Object.defineProperty(Vue.prototype, $router, {
+        get () {
+            return this.$options.router
+        }
+    })
+    Object.defineProperty(Vue.prototype, $route, {
+        get () {
+            return this.$router.getRoute()
+        }
+    })
+
     Vue.component('RouterView', RouterView)
     Vue.component('RouterLink', RouterLink)
     
